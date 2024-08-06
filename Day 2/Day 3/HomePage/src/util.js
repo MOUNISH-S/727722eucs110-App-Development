94c5c0da9bddad7be6ad8e350ffda1d57924e3cd
MOUNISH-S <125254315+MOUNISH-S@users.noreply.github.com>
2024-08-06 14:10:43 +0530

    Delete Day 2/Day 3/HomePage/src directory

--- Day 2/Day 3/HomePage/src/util.js
+++
@@ -1,6 +0,0 @@
-// src/utils.js
-export const shouldShowNavbarAndFooter = (pathname) => {
-    const pathsWithoutNavbarAndFooter = ['/login', '/register', '/checkout'];
-    return !pathsWithoutNavbarAndFooter.includes(pathname);
-  };
-  
